66da7616dbc2218c961f55f6c6be5c5ad6d7b153
pythonista0801 <natalia.chetveryk@gmail.com>
2021-01-31 22:58:30 +0200

    Name of the variable corrected.

--- Homework#4.py
+++ Homework#4.py
@@ -106,10 +106,10 @@ for symb_1 in my_string_1:
 Задание нужно выполнить ТОЛЬКО через цикл в цикле(См. пример выше) и приведение типов.
 Генерирование через range или другие "фишки" я засчитывать не буду ))"""
 
-my_string_1 = '0123456789'
+my_string = '0123456789'
 result = []
-for symb_1 in my_string_1:
-	for symb_2 in my_string_1:
+for symb_1 in my_string:
+	for symb_2 in my_string:
 		result.append(symb_1 + symb_2)
 print(result)
 
