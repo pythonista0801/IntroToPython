9a4077c1e1e2dcc16bb08ef29ef0d4187b7d589e
pythonista0801 <natalia.chetveryk@gmail.com>
2021-02-09 21:55:16 +0200

    Задания 1, 2, 3, 4а,б, 5 а,б

--- Homework#7.py
+++ Homework#7.py
@@ -46,9 +46,50 @@ print_string(my_str)
 в) Посчитать среднее количество лет всех людей из списка.
 """
 
+persons = [{"name": "John", "age": 25},
+           {"name": "Anna", "age": 65},
+            {"name": "Bill", "age": 33},
+            {"name": "Jack", "age": 45},
+           {"name": "Christine", "age": 15},
+           {"name": "Kateline", "age": 15}]
+
+"""а) Напечатать имя самого молодого человека. Если возраст совпадает - напечатать все имена."""
+
+all_age = []
+for person in persons:
+    all_age.append(person["age"])
+print(all_age)
+
+min_age = min(all_age)
+print(min_age)
+
+for item in persons:
+    if item["age"] > min_age:
+        pass
+    elif item["age"] == min_age:
+        print(f"Имя самого молодого человека - {item["name"]}")
+
+print("------------------------------------------------------------------")
+
+"""б) Напечатать самое длинное имя. Если длина имени совпадает - напечатать все имена."""
+
+all_names = []
+for person in persons:
+    all_names.append(person["name"])
+    print(all_names)
+
+for i in all_names:
 
 
+"""Если циклом пройтись по persons и прочитать person["age"] 
+в отдельный список и потом найти min для него. 
+Это будет минимальный возраст.
+А потом еще раз в цикле пройти и сравнить с этим значением."""
 
+#for d in LofD:
+  #   for k, v in d.items():
+  #     r[k][v] += 1
+  # return dict((k, dict(v)) for k, v in r.items())
 
 """
 5) Даны два словаря my_dict_1 и my_dict_2.
@@ -59,3 +100,37 @@ print_string(my_str)
 если ключ есть только в одном из двух словарей - поместить пару ключ:значение,
 если ключ есть в двух словарях - поместить пару {ключ: [значение_из_первого_словаря, значение_из_второго_словаря]},
 """
+my_dict_1 = {"name": "John",
+             "last_name": "Black",
+             "email": "test@gmail.com",
+             "age": 35,
+             "address": "Cleveroad, 12",
+             "Company": "Soft Solutions"}
+
+my_dict_2 = {"name": "David",
+             "email": "david@gmail.com",
+             "age": 45,
+             "address": "Bendroad, 18",
+             "Occupation": "Developer",
+             "Hobby": "Sky Diving"}
+
+"а) Создать список из ключей, которые есть в обоих словарях."
+
+my_dict_1_keys = set(my_dict_1.keys())
+my_dict_2_keys = set(my_dict_2.keys())
+
+list_of_keys = list(my_dict_1_keys.intersection(my_dict_2_keys))
+print(list_of_keys)
+
+"б) Создать список из ключей, которые есть в первом, но нет во втором словаре."
+
+list_of_unique_keys = []
+for key in my_dict_1_keys:
+    if key not in my_dict_2_keys:
+        list_of_unique_keys.append(key)
+print(list_of_unique_keys)
+
+"в) Создать новый словарь из пар {ключ:значение}, для ключей,"
+"которые есть в первом, но нет во втором словаре."
+
+new_dict = {}
