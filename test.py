7646231123ffcb03584e79dd6d57cdd021bd11fb
pythonista0801 <natalia.chetveryk@gmail.com>
2021-02-24 19:56:03 +0200

    Test file to try out different solutions

--- test.py
+++ test.py
@@ -1,9 +1,37 @@
-import random as rnd
+import json
+import random
 import string
 
-def random_string(min_limit: int, max_limit: int):
-    selection = [rnd.choice(string.ascii_lowercase) for i in range(rnd.randint(min_limit, max_limit))]
-    str = ''.join(selection)
-    return str
+"""
+3. Написать функцию generate_and_write_json которая принимает один параметр - полный путь к файлу.
+Cгенерировать данные для записи с помощью функции из пункта 2 и записать в данный файл.
+"""
+# def write_txt(file_path, data):
+#     with open(file_path, "w") as txt_file:
+#         txt_file.writelines(data)
 
-print(random_string(35, 70))
+path = "C:\\Users\\CNata\\PycharmProjects\\IntroToPython\\"
+
+filepath = "C:\\Users\\CNata\\PycharmProjects\\IntroToPython\\my_dict.json"
+
+def generate_and_write_json(filepath):
+    keys = []
+    values = []
+
+    with open(filepath, "w") as my_json:
+        json.dump(my_dict, my_json)
+    # return filepath
+
+generate_and_write_json(filepath)
+
+filename = "my_dict_write.json"
+with open(filename, "w") as js_file:
+    json.dump(my_dict, js_file, indent = 2)
+
+file_path='/tmp/student_data.json'
+    with open(file_path, 'w') as outfile:
+        print("writing file to: ",file_path)
+        # HERE IS WHERE THE MAGIC HAPPENS
+        json.dump(student_data, outfile)
+
+
