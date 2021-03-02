34d8a362b3c2204e803ccc9b5c002f1dae29caa2
pythonista0801 <natalia.chetveryk@gmail.com>
2021-03-02 16:47:16 +0200

    Замечания учтены, доработка HW

--- Homework#10.py
+++ Homework#10.py
@@ -8,25 +8,34 @@ import json
 Если расширение не соответствует заданным, то вывести текст "Unsupported file format"
 """
 
+def read_txt(file_path):
+    with open(file_path, 'r') as txt_file:
+        data = [line for line in txt_file.readlines()]
+    return data
+
+def read_csv(file_path):
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        data = [row for row in reader]
+    return data
+
+def read_json(file_path):
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
 def read_file(file_path):
     extension = file_path.rsplit('.',1)[-1]
+
     if extension == "txt":
-        with open(file_path, 'r') as txt_file:
-             data = []
-             for line in txt_file.readlines():
-                 data.append(line)
-        return data
+        read_txt(file_path)
+
     elif extension == "csv":
-        with open(file_path, 'r') as csv_file:
-            data = []
-            reader = csv.reader(csv_file, delimiter = ';')
-            for row in reader:
-                data.append(row)
-        return data
+        read_csv(file_path)
+
     elif extension == "json":
-        with open(file_path, 'r') as json_file:
-            data = json.load(json_file)
-        return data
+        read_json(file_path)
+
     else:
         print("Unsupported file format")
 
@@ -57,28 +66,39 @@ print(data)
 Данные для записи и файлы создать самим. Файлы с результатами добавить в гит.
 """
 
+def write_txt(filename, data):
+    with open(filename, "w") as txt_file:
+        txt_file.writelines(data)
+
+def write_json(filename, data):
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=2)
+
+def write_csv(filename, data):
+    with open(filename, "w", encoding="utf-8") as csv_file:
+        fieldnames = data[0].keys()
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
 def write_file(filename, data):
     extension = filename.rsplit(".", 1)[-1]
     if extension == "txt":
-        with open(filename, "w") as txt_file:
-            txt_file.writelines(data)
+        write_txt(filename, data)
+
     elif extension == "json":
-        with open(filename, "w") as json_file:
-            json.dump(data, json_file, indent = 2)
+        write_json(filename, data)
+
     elif extension == "csv":
-        with open(filename,"w", encoding="utf-8") as csv_file:
-            fieldnames = data[0].keys()
-            writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
-            writer.writeheader()
-            writer.writerows(data)
+        write_csv(filename, data
+                  )
     else:
         print("Unsupported file format")
 
-
-data = "aajgkajhakfj aljhlgkjhaskjf ;adlkfkajhfkajdh"
-filename = "newhw10.txt"
-write_file(filename, data)
-print(read_file("newhw10.txt"))
+# data = "aajgkajhakfj aljhlgkjhaskjf ;adlkfkajhfkajdh"
+# filename = "newhw10.txt"
+# write_file(filename, data)
+# print(read_file("newhw10.txt"))
 
 write_file(filename, "I hope that works out well")
 print(read_file("newhw10.txt"))
